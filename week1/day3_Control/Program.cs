b58531480b7f469e6d90a16105f0b3f39388fd4d
Jonathan Medrano <jonathanmedranoo41@gmail.com>
2025-07-23 19:41:45 -0300

    day3

---
+++ week1/day3_Control/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un número:");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100)
+{
+    Console.WriteLine("Número muy grande");
+    Console.ReadKey();
+}
+else
+{
+
+    switch (number % 2)
+    {
+        case 0:
+            Console.WriteLine("Es Par");
+            break;
+        case > 0:
+            Console.WriteLine("Es Par");
+            break;
+    }
+}
