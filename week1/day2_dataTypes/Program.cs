90e43d58c1ac23badb3e1e00474d25b29198e083
Jonathan Medrano <jonathanmedranoo41@gmail.com>
2025-07-22 21:00:05 -0300

    Add .gitignore and remove .vs folder

---
+++ week1/day2_dataTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese su nombre:");
+string username = Console.ReadLine();
+Console.WriteLine("Ingrese su edad:");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese su estatura en centimetros:");
+float height = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Usted se llama {username}, tiene {age} años y mide {height / 100}mts.");
+Console.ReadKey();
